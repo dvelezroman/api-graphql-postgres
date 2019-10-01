e2d7afa00e99bf69176558503a21f00123679086
Dario Velez Roman <dario.velez@dinmax.com>
2019-10-01 17:23:10 -0300

    ready connected to postgres in DO

--- envs.js
+++ envs.js
@@ -9,13 +9,21 @@ const envs = () => {
 			DB_NAME: 'seguros',
 			DB_USERNAME: 'puesto1',
 			DB_PASSWORD: 'mentaleche2304',
-			DB_HOSTNAME: 'localhost'
+			DB_HOSTNAME: '127.0.0.1',
+			DB_PORT: 5432
 		};
 	} else {
 		return {
-			SERVERURL: 'https://app.arsys.es/',
-			PORT: 5000,
-			JWT_SECRET: 'caffeinasw'
+			SERVERURL: 'http://localhost:3000/',
+			PORT: 3000,
+			JWT_SECRET: 'caffeinasw',
+			DB_NAME: 'seguros',
+			DB_USERNAME: 'doadmin',
+			DB_PASSWORD: 'i9wo9refuagzdbud',
+			DB_HOSTNAME:
+				'db-postgresql-nyc1-68324-do-user-6604645-0.db.ondigitalocean.com',
+			DB_PORT: 25060,
+			sslmode: require
 		};
 	}
 };
--- service/db.js
+++ service/db.js
@@ -1,10 +1,17 @@
 const Sequelize = require('sequelize');
 const envs = require('../envs')();
+const fs = require('fs');
 
 const db = new Sequelize(envs.DB_NAME, envs.DB_USERNAME, envs.DB_PASSWORD, {
 	host: envs.DB_HOSTNAME,
 	logging: false,
-	dialect: 'postgres'
+	dialect: 'postgres',
+	dialectOptions: {
+		ssl: {
+			ca: fs.readFileSync(__dirname + '/certs/ca-certificate.crt')
+		}
+	},
+	port: envs.DB_PORT
 });
 
 module.exports = db;
