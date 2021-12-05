8467c16a582f6d96ab09a11acd15b83924156136
Dario Velez Roman <dvelezroman@gmail.com>
2021-12-05 01:46:43 -0300

    add build folder

---
+++ build/precache-manifest.14da5066a71a29746eb7e4d719825477.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ae85971609e2082d43a6762f3ba4da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "485fd9fd6efb28689ce4",
+    "url": "/static/css/2.a749387b.chunk.css"
+  },
+  {
+    "revision": "88114eae87f4ed585ae3",
+    "url": "/static/css/main.242ee0b4.chunk.css"
+  },
+  {
+    "revision": "485fd9fd6efb28689ce4",
+    "url": "/static/js/2.4cd9e529.chunk.js"
+  },
+  {
+    "revision": "88114eae87f4ed585ae3",
+    "url": "/static/js/main.26dd7136.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "af300b6e21dff8e136aa4f4acdb8dd11",
+    "url": "/static/media/daniel-olahs.af300b6e.jpg"
+  },
+  {
+    "revision": "8f9581dbf863fcb3494a8bbe0f89d6eb",
+    "url": "/static/media/david-marcu.8f9581db.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "7907591946eee451e53d97d92cc238c9",
+    "url": "/static/media/react-logo.79075919.png"
+  }
+]);
