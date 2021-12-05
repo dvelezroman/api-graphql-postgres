6169ff3a1064cf7ac14899c584124f65d8aa4218
Dario Velez Roman <dvelezroman@gmail.com>
2021-12-04 23:11:19 -0300

    new creds

--- envs.js
+++ envs.js
@@ -12,7 +12,7 @@ const envs = () => {
 			DB_USERNAME: 'root',
 			DB_PASSWORD: 'root2021',
 			DB_HOSTNAME: '127.0.0.1',
-			DB_PORT: 3036,
+			DB_PORT: 3306,
 			// sslmode: require,
 		};
 	} else {
@@ -22,10 +22,10 @@ const envs = () => {
 			PORT: 3005,
 			JWT_SECRET: 'caffeinasw',
 			DB_NAME: 'my_crm_db',
-			DB_USERNAME: 'root',
-			DB_PASSWORD: 'root2021',
+			DB_USERNAME: 'dvelez',
+			DB_PASSWORD: 'dvelez2021',
 			DB_HOSTNAME: '127.0.0.1',
-			DB_PORT: 3036,
+			DB_PORT: 3306,
 			// sslmode: require,
 		};
 	}
